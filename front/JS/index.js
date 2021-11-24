d49c409750114584c33fc3213700179b423d1806
sparta40700 <valentinmercier.40@gmail.com>
2021-11-24 09:53:22 +0100

    start

---
+++ front/JS/index.js
@@ -0,0 +1,6 @@
+//console.log(aide à debuguer)
+// reccuperer les produits cote serveur via l'adresse http://localhost:3000/api/products
+
+//boucler (for each) sur la variables qui contient tout les produits
+//reccuperer la section "id" "items"
+//chaque boucle creer le code html pour l'injecter
